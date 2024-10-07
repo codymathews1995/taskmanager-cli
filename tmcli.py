8c3d14b9f3e942823c8a1df2513b0eb975bd1e2a
codymathews1995 <codymathews1995@gmail.com>
2024-10-06 22:13:14 -0700

    Added tmcli.py

---
+++ tmcli.py
@@ -0,0 +1,22 @@
+class task:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+        self.completed = False
+    
+    def __str__(self):
+        pass
+    
+    def mark_completed():
+        pass
+    
+    def to_dict(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
+    
+    
